770bb04c582c259825e28b4408c8d5f0d058ea0c
Zhuridochka <zhuridochka@gmail.com>
2024-11-14 11:27:28 +0200

    Fixed

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ import { plugins } from "./config/gulp-plugins.js";
 // Імпорт шляхів
 import { pathtofiles } from "./config/gulp-settings.js";
 // Імпорт функціоналу NodeJS
-import fs from 'fs';
+import fs from "fs";
 
 // Передаємо значення у глобальну змінну
 global.app = {
-	isBuild: process.argv.includes('--build'),
-	isDev: !process.argv.includes('--build'),
-	isWebP: !process.argv.includes('--nowebp'),
-	isImgOpt: !process.argv.includes('--noimgopt'),
-	isFontsReW: process.argv.includes('--rewrite'),
-	gulp: gulp,
-	path: pathtofiles,
-	plugins: plugins
-}
+  isBuild: process.argv.includes("--build"),
+  isDev: !process.argv.includes("--build"),
+  isWebP: !process.argv.includes("--nowebp"),
+  isImgOpt: !process.argv.includes("--noimgopt"),
+  isFontsReW: process.argv.includes("--rewrite"),
+  gulp: gulp,
+  path: pathtofiles,
+  plugins: plugins,
+};
 
 // Імпорт завдань
 import { reset } from "./config/gulp-tasks/reset.js";
@@ -30,38 +30,66 @@ import { ftp } from "./config/gulp-tasks/ftp.js";
 import { zip } from "./config/gulp-tasks/zip.js";
 import { sprite } from "./config/gulp-tasks/sprite.js";
 import { gitignore } from "./config/gulp-tasks/gitignore.js";
-import { otfToTtf, ttfToWoff2, woff2Copy, fontsStyle } from "./config/gulp-tasks/fonts.js";
+import {
+  otfToTtf,
+  ttfToWoff2,
+  woff2Copy,
+  fontsStyle,
+} from "./config/gulp-tasks/fonts.js";
 
 // Послідовна обробка шрифтів
 const fonts = gulp.series(reset, function (done) {
-	// Якщо існує папка fonts
-	if (fs.existsSync(`${app.path.srcFolder}/fonts`)) {
-		gulp.series(otfToTtf, ttfToWoff2, woff2Copy, fontsStyle)(done);
-	} else {
-		done();
-	}
+  // Якщо існує папка fonts
+  if (fs.existsSync(`${app.path.srcFolder}/fonts`)) {
+    gulp.series(otfToTtf, ttfToWoff2, woff2Copy, fontsStyle)(done);
+  } else {
+    done();
+  }
 });
 
 // Порядок виконання завдань для режиму розробник
 const devTasks = gulp.series(fonts, gitignore);
 // Порядок виконання завдань для режиму продакшн
 let buildTasks;
-if (process.argv.includes('--nowebp')) {
-	buildTasks = gulp.series(fonts, jsDev, js, gulp.parallel(html, css, gulp.parallel(WebP, imagesOptimize, copySvg), gitignore));
+if (process.argv.includes("--nowebp")) {
+  buildTasks = gulp.series(
+    fonts,
+    jsDev,
+    js,
+    gulp.parallel(
+      html,
+      css,
+      gulp.parallel(WebP, imagesOptimize, copySvg),
+      gitignore
+    )
+  );
 } else {
-	buildTasks = gulp.series(fonts, jsDev, js, gulp.parallel(html, css, gulp.parallel(WebP, copySvg), gitignore));
+  buildTasks = gulp.series(
+    fonts,
+    jsDev,
+    js,
+    gulp.parallel(html, css, gulp.parallel(WebP, copySvg), gitignore)
+  );
 }
 
+// Додавання файлів pdf
+const gulp = require("gulp");
+const copy = require("gulp-copy");
+gulp.task("copy-resume", function () {
+  return gulp.src("public/*.pdf").pipe(gulp.dest("dist/public/"));
+});
+
+gulp.task("build", gulp.series("copy-resume"));
 
 // Експорт завдань
-export { html }
-export { css }
-export { js }
-export { jsDev }
-export { fonts }
-export { sprite }
-export { ftp }
-export { zip }
+export { html };
+export { css };
+export { js };
+export { jsDev };
+export { fonts };
+export { sprite };
+export { ftp };
+export { zip };
 
 // Побудова сценаріїв виконання завдань
 const development = devTasks;
@@ -70,16 +98,10 @@ const deployFTP = gulp.series(buildTasks, ftp);
 const deployZIP = gulp.series(buildTasks, zip);
 
 // Експорт сценаріїв
-export { development }
-export { build }
-export { deployFTP }
-export { deployZIP }
+export { development };
+export { build };
+export { deployFTP };
+export { deployZIP };
 
 // Виконання сценарію за замовчуванням
-gulp.task('default', development);
-
-
-
-
-
-
+gulp.task("default", development);
